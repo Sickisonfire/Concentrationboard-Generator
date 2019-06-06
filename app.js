cc2d920f89ad3a29758dc0beeca9ff6bb86535bd
Marcel <firebird10@gmx.de>
2019-06-06 19:02:50 +0200

    todo: generate rows

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-document.getElementById('generate').addEventListener('click', createArray);
+document.getElementById('generate').addEventListener('click', createBoard);
+const size = parseInt(document.getElementById('sizeinput').value);
 
+//shuffle array funktion
 const shuffle = array => {
   let currentIndex = array.length;
   let temporaryValue, randomIndex;
@@ -18,14 +20,33 @@ const shuffle = array => {
 
   return array;
 };
-function createArray() {
-  let size = parseInt(document.getElementById('sizeinput').value);
+
+//create the board
+function createBoard() {
   //console.log(typeof size);
+  //remove old board
+  document.getElementById('boardtable').remove();
+
+  //create table
+  const table = document.createElement('table');
+  table.id = 'boardtable';
+  console.log(table);
+  document.getElementById('board-container').appendChild(table);
+  const size = parseInt(document.getElementById('sizeinput').value);
   //generate Array
   let arr = Array.from(Array(size).keys());
 
+  //generate shuffledarray and add 1 to all
   let arrShuffled = shuffle(arr).map(x => x + 1);
 
-  document.getElementById('output').innerHTML = arrShuffled;
+  for (let i = 0; i < arrShuffled.length; i++) {
+    let board;
+    let td = document.createElement('td');
+    td.innerText = arrShuffled[i];
+
+    table.appendChild(td);
+
+    document.getElementById('output').innerHTML = arrShuffled;
+  }
   console.log(arrShuffled);
 }
